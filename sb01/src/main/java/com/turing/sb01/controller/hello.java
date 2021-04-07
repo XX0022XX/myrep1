4dc32f99c5319fa30073306cfd0f8329b3319054
XX <XX@qq.com>
2021-04-07 17:08:48 +0800

    sb01第一次提交

---
+++ sb01/src/main/java/com/turing/sb01/controller/hello.java
@@ -0,0 +1,14 @@
+package com.turing.sb01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class hello {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String  show(){
+        return "hello";
+    }
+}
